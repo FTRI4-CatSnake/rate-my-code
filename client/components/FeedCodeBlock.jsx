7fbd09c896c7bb08acafd79474d15828a0cf4696
Jackie Whitworth <jackie.whitworth@gmail.com>
2021-10-06 15:49:20 -0500

    Updated state in CreatePost.jsx and Feed.jsx, added title to FeedCodeBlock.jsx

--- client/components/FeedCodeBlock.jsx
+++ client/components/FeedCodeBlock.jsx
@@ -8,9 +8,10 @@ export default function FeedCodeBlock(props) {
     <section>
       {/* update the link to /postview/${props.info.id} to get the post id from props and redirect there */}
       <Link to={'/home/postview/'}>
-        <div className={classes.codeBlock}>
+        <div className='codeBlock'> {/*Changed class from {classes.codeBlock}*/}
           <div>
-            {props.code}
+            <h2>{props.title}</h2>
+            <code>{props.code}</code>
           </div>
         </div>
       </Link>
