e985f1f6a8058eadade6fa1382c5ce57c6e447fc
Jackie Whitworth <jackie.whitworth@gmail.com>
2021-10-07 12:11:21 -0500

    Worked on MUI theme for Feed page

--- client/components/ThemedFeed.jsx
+++ client/components/ThemedFeed.jsx
@@ -25,10 +25,38 @@ export default function ThemedFeed() {
       <AppBar postition='relative'>
         <Toolbar>
           <Typography variant='h6' colors='inherit' noWrap>
-            Welcome to Rate-My-Code
+            Filter Posts by Topic
           </Typography>
         </Toolbar>
       </AppBar>
+
+      <main>
+        {/*Hero*/}
+        <Box
+          sx={{
+            bgcolor: 'Background.paper',
+            pt: 8,
+            pb: 6,
+          }}
+        >
+          <Container maxWidth='sm'>
+            <Typography 
+              component='h1'
+              variant='h2'
+              align='center'
+              color='text.primary'
+              gutterBottom
+            >
+              Welcome to Rate-My-Code
+            </Typography>
+            <Typography variant='h5' align='center' color='text.secondary' paragraph>
+
+            </Typography>
+
+          </Container>
+        </Box>
+
+      </main>
 
     </ThemeProvider>
 
